dc0b177981beb04e33067772575dd76ec38e165d
shoji4638 <56951980+shoji4638@users.noreply.github.com>
2023-08-29 21:04:41 +0900

    Tagの入力規制
    TagIDの入力規制

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 import Marklin_Control.main      #Flaskを動かすデレクトリ名
 
 from Marklin_Control import db
-db.create_books_table()
+db.create_books_table()             #立上時にデータベースなかったら作成⇒db.pyを実行
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from Marklin_Control import app
 from flask import render_template, request, redirect,url_for
 import sqlite3
+import re
 DATABASE = 'marklin.db'
+
+#@app.route('/')
+#def index():
+#
+#    return render_template(
+#        'index.html'
+#    )
 
 @app.route('/')
 def index():
@@ -10,20 +18,45 @@ def index():
         'index.html'
     )
 
+
+@app.route('/posttest',methods = ['POST'])
+def posttest():
+
+    try:
+        print(request.form['action'],request.form['action'][4:])
+        con = sqlite3.connect(DATABASE)
+        db_trains = con.execute("delete from trains where id = '{}'".format(request.form['action'][4:]))
+        print(db_trains)
+        con.commit()
+        con.close()
+        return redirect(url_for('database'))
+
+    except:
+        print(request.form['change'])
+        
+        return redirect(url_for('database', methods = 'ABC'))
+
+#    print(request.form['change'])
+#    return redirect(url_for('index'))
+
 @app.route('/database')
 def database():
+
+#    print(request.form['mothods'])
 
     con = sqlite3.connect(DATABASE)
     db_trains = con.execute('SELECT * FROM trains').fetchall()
     con.close()
 
     trains = []
-    for row in db_trains:
+    for i,row in enumerate(db_trains):
         trains.append({
-            'type': row[0],
-            'name': row[1],
-            'length': row[2],
-            'picture': row[3]})
+            'id': row[0],
+            'type': row[1],
+            'name': row[2],
+            'length': row[3],
+            'picture': row[4],
+            'num': i})
 
     return render_template(
         'database.html',
@@ -32,15 +65,23 @@ def database():
 
 @app.route('/registar', methods=['POST'])
 def register():
+    id = request.form['id']
     type = request.form['type']
     name = request.form['name']
     length = request.form['length']
     picture = request.form['picture']
 
-    con = sqlite3.connect(DATABASE)
-    con.execute('INSERT INTO trains VALUES(?, ?, ?, ?)',
-                [type, name, length, picture])
-    con.commit()
-    con.close()
+    p = re.compile('[0-9a-zA-Z]+')
+    print(p.fullmatch(id).group())
+    if len(p.fullmatch(id).group()) == 10:
+        print('Length Match!!!!')
+
+        con = sqlite3.connect(DATABASE)
+        con.execute('INSERT INTO trains VALUES(?, ?, ?, ?, ?)',
+                [id, type, name, length, picture])
+        con.commit()
+        con.close()
+    else:
+        print('入力エラー')
 
     return redirect(url_for('database'))
